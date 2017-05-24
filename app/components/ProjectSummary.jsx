4b1764e242797475fa6213cb4ac8eecdcdfb5f3f
Thomas Huisking <tomasfaustin@gmail.com>
2017-05-24 14:36:13 -0700

    Update summary jsx and scss

--- app/components/ProjectSummary.jsx
+++ app/components/ProjectSummary.jsx
@@ -1,38 +1,57 @@
 import React from 'react'
 
 class ProjectSummary extends React.Component {
-	constructor(props) {
-		super(props)
-	}
-	render() {
-		var {color, name, due_date, created_at, current_status, modified_at, id, members} = this.props.project
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        var {
+            color,
+            name,
+            due_date,
+            created_at,
+            current_status,
+            modified_at,
+            id,
+            members
+        } = this.props.project
 
-		if (!color) color = 'green'
+        if (!color) {
+            current_status
+                ? color = current_status.color
+                : color = 'green'
+        }
 
-		var renderDate = (date) => {
-			return new Date(date).toDateString()
-		}
+        if (current_status) {
+					var text = current_status.text;
+				} else {
+					var text = 'No update available.'
+				}
+        var renderDate = (date) => {
+            return new Date(date).toDateString()
+        }
 
-		var renderColor = (color) => {
-			return new Color(color)
-		}
+        // var renderColor = (color) => {
+        // 	return new Color(color)
+        // }
 
-		var teamMembersList = members.map((member, i, arr) => {
-			if (i === arr.length - 1) return `and ${member.name}.`
-			return `${member.name}, `
-		})
+        var teamMembersList = members.map((member, i, arr) => {
+            if (i === arr.length - 1)
+                return `and ${member.name}.`
+            return `${member.name}, `
+        })
 
-		return (
-			<li className={`${color} project-summary`} onClick={() => this.props.handleClick(this.props.project) }>
-				<h2>{name}</h2>
-				<p>Remaining  | Completed </p>
-				<p>Team: {teamMembersList}</p>
-				<p>Updated: {renderDate(modified_at)}</p>
-			</li>
-		)
-	}
+        return (
+            <li className={`${color} project-summary`} onClick={() => this.props.handleClick(this.props.project)}>
+                <h2>{name}</h2>
+                <p>Remaining | Completed
+                </p>
+                <p>Team: {teamMembersList}</p>
+                <p>Updated: {renderDate(modified_at)}</p>
+                <p>Description: {text}</p>
+            </li>
+        )
+    }
 }
-
-
 
 export default ProjectSummary
