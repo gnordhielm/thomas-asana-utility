71ab19c64515137cab5a4b2ec99155616b12778f
Thomas Huisking <tomasfaustin@gmail.com>
2017-05-24 19:16:45 -0700

    Update asana.jsx for development

--- app/api/asana.jsx
+++ app/api/asana.jsx
@@ -3,5 +3,5 @@ var Asana = require('asana')
 // Configuration - move out to .env file
 export const CLIENT_ID = '344321689330782'
 export const CLIENT_SECRET = '3bc83775e039d433df8c48a275387f30'
-export const REDIRECT_URI = 'https://aqueous-crag-19153.herokuapp.com/projects'
-export const AUTH_ENDPOINT = `https://app.asana.com/-/oauth_authorize?response_type=code&client_id=${344321689330782}&redirect_uri=https%3A%2F%2Faqueous-crag-19153.herokuapp.com%2Fprojects`
+export const REDIRECT_URI = 'https://localhost:5000/'
+export const AUTH_ENDPOINT = 'https://app.asana.com/-/oauth_authorize?response_type=code&client_id=${344321689330782}&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fprojects'
